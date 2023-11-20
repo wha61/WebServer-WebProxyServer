e584a733cd5a793e853024e72e5daab1d2b0f03e
wha61 <105824798+wha61@users.noreply.github.com>
2023-11-20 15:06:24 -0800

    implemnent message and corresponding testing

--- web_client.py
+++ web_client.py
@@ -7,9 +7,10 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 
 clientSocket.connect((serverName, serverPort))
 
-requestOne = "GET /test.html HTTP/1.1\r\nHost: '127.0.0.1'\r\n"
+requestOne = "GET /test.html HTTP/1.1\r\n Host: '127.0.0.1'\r\n"
 byteLength = len(requestOne.encode("utf-8"))
-contentLength = f"Content-Length:{byteLength}\r\n"
+# contentLength = f"Content-Length:{byteLength}\r\n"
+contentLength = f""
 requestList = [requestOne, contentLength]
 fullRequest = " ".join(requestList)
 print(fullRequest)
--- web_server.py
+++ web_server.py
@@ -12,7 +12,10 @@ serverSocket = socket(AF_INET,SOCK_STREAM)
 
 forbiddenList = ["secret.html"]
 
+supported_methods = ["GET", "POST", "PUT", "DELETE"] 
+
 # Bind the server address & server port to the socket
+# localhost/8080:
 serverSocket.bind(('127.0.0.1',serverPort))
 
 # Server begins listerning for incoming TCP connections
@@ -28,52 +31,76 @@ while True:  # Loop forever
     try:
         # Receive request message from client
         message = connectionSocket.recv(1024).decode('utf-8')
-        print('The request message from client:', message)
+        print('The request message from client:', "\n", message)
 
-        if not message:
-            raise ValueError("Empty request received")
-
-        message_parts = message.split()
-
-        if len(message_parts) < 2:
-            raise ValueError("Invalid HTTP request format")
+        # case 400-Bad Request(not yet complete)
+        message_parts = message.split("\r\n")
+        message_part = message_parts[0] if message_parts else ''
+        # test use: 
+        if not message_part or len(message_part.split()) != 3:
+            raise ValueError("Invalid request line")
+        request_method, request_path, request_protocol = message_part.split()
+        # test use: curl -i -X BOGUS http://localhost:8080/test.html
+        if request_method not in supported_methods:
+            raise ValueError(f"Unsupported method: {request_method}")
+        # test use: 
+        for header in message_parts[1:]:
+            if header and ":" not in header:
+                raise ValueError(f"Invalid header format: {header}")
 
         # From message get file name
-        testFile = message_parts[1][1:]
-        print('The test file from client:', testFile)
+        testFile = message_part.split()[1][1:]
+        print('The test file request from client:', testFile, "\n")
 
         # Get metadata last modified of the testfile we have in our OS.
         lastModified = os.path.getmtime(testFile)
         lastModifiedString = formatdate(timeval = lastModified, localtime = False, usegmt = True)
+        print("Last Modify time:", lastModifiedString, "\n")
 
-        if "If-Modified-Since" in message:
-            modifiedTimeString = message.split("If-Modified-Since")[1].split()[0]
+        # case 304-Not Modified (implemented)
+        # the last modfied time of modified.html is Sun, 19 Nov 2023 07:10:11 GMT
+        # To test, run following 2 commands in cmd:
+        # 1.modifiedTime < lastModified, should return 200 OK:
+        # curl -i http://localhost:8080/modified.html -H "If-Modified-Since: Sat, 21 Oct 2023 07:28:00 GMT"
+        # 2.modifiedTime >= lastModified, should return 304 Not Modified:
+        # curl -i http://localhost:8080/modified.html -H "If-Modified-Since: Sat, 21 Dec 2023 07:28:00 GMT"
+        if "If-Modified-Since:" in message:
+            modifiedTimeString = message.split("If-Modified-Since:")[1]
             modifiedTime = parsedate(modifiedTimeString)
             if lastModified <= time.mktime(modifiedTime):
                 connectionSocket.send("HTTP/1.1 304 Not Modified\r\n\r\n".encode())
                 connectionSocket.close()
                 continue
 
-        if "Content-Length" not in message:
-            connectionSocket.send("HTTP/1.1 411 Length Required\r\n\r\n".encode())
-            connectionSocket.close()
-            continue
-
+        # case 403-Forbidden(implemented)
         if testFile in forbiddenList:
             connectionSocket.send("HTTP/1.1 403 Forbidden\r\n\r\n".encode())
             connectionSocket.close()
             continue
 
-        print(testFile)
+        # case 411-Length Required(implemented)
+        # To test, run following 2 commands in cmd:
+        # 1.no content length: curl -i -X POST http://localhost:8080/test.tml -H "Content-Type: application/x-www-form-urlencoded"
+        # 2.with content length: curl -i -X POST http://localhost:8080/test.html -H "Content-Type: application/x-www-form-urlencoded" -d "test value"
+        requires_content_length = False
+        request_method, request_path, request_protocol = message_part.split()
+        print(f"Request Method: {request_method}")  # 添加这行来检查请求方法
+        # GET usually do not have Content-Length section, 
+        # so check when get POST or PUT request
+        requires_content_length = request_method in ["POST", "PUT"]
+        content_length_present = "Content-Length:" in message
+        if requires_content_length and not content_length_present:
+            connectionSocket.send("HTTP/1.1 411 Length Required\r\n\r\n".encode())
+            connectionSocket.close()
+            continue
+
         # Open file and get html data
         with open(testFile, 'rb') as file:
             testHTML = file.read()
 
-        # Send HTTP response header to client
+        # case 200-OK(implemented)
         connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-
         connectionSocket.send(testHTML)
-
         connectionSocket.close()
 
     except IOError:
@@ -85,5 +112,6 @@ while True:  # Loop forever
         print(e)
         connectionSocket.send("HTTP/1.1 400 Bad Request\r\n\r\n".encode())
         connectionSocket.close()
+        continue
     except KeyboardInterrupt:
         connectionSocket.close()
